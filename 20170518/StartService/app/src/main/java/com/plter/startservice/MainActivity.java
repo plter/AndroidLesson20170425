bf63c6c5491404ed897bda05203c4c1946b11f5b
plter <xtiqin@163.com>
2017-05-18 10:29:21 +0800

    使用startService影响服务内部状态

--- 20170518/StartService/app/src/main/java/com/plter/startservice/MainActivity.java
+++ 20170518/StartService/app/src/main/java/com/plter/startservice/MainActivity.java
@@ -7,19 +7,33 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Intent myServiceIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myServiceIntent = new Intent(this, MyService.class);
+
         findViewById(R.id.btnStartService).setOnClickListener(this);
+        findViewById(R.id.btnStopService).setOnClickListener(this);
+        findViewById(R.id.btnSetServiceCountTo100).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnStartService:
-                startService(new Intent(this, MyService.class));
+                startService(myServiceIntent);
+                break;
+            case R.id.btnStopService:
+                stopService(myServiceIntent);
+                break;
+            case R.id.btnSetServiceCountTo100:
+                Intent intent = new Intent(this, MyService.class);
+                intent.putExtra("count", 100);
+                startService(intent);
                 break;
         }
     }
--- 20170518/StartService/app/src/main/java/com/plter/startservice/MyService.java
+++ 20170518/StartService/app/src/main/java/com/plter/startservice/MyService.java
@@ -6,6 +6,10 @@ import android.os.IBinder;
 import android.support.annotation.IntDef;
 
 public class MyService extends Service {
+
+    private boolean running = false;
+    private int count = 0;
+
     public MyService() {
     }
 
@@ -19,12 +23,39 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
 
+        running = true;
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+
+                while (running) {
+                    try {
+                        sleep(1000);
+
+                        count++;
+                        System.out.println(count);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                }
+            }
+        }.start();
+
         System.out.println("MyService.onCreate");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         System.out.println("MyService.onStartCommand");
+
+        int count = intent.getIntExtra("count", 0);
+        if (count != 0) {
+            this.count = count;
+        }
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -32,5 +63,7 @@ public class MyService extends Service {
     public void onDestroy() {
         System.out.println("MyService.onDestroy");
         super.onDestroy();
+
+        running = false;
     }
 }
