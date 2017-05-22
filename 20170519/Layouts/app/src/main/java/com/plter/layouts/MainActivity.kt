4fa0f08216d7f50aedcd09907694466b3d37dc14
plter <xtiqin@163.com>
2017-05-22 08:58:53 +0800

    使用代码动态改变布局

---
+++ 20170519/Layouts/app/src/main/java/com/plter/layouts/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.plter.layouts;
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+
+open class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        val parent = v?.parent as LinearLayout
+        parent.removeView(v)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        val view = LayoutInflater.from(this).inflate(R.layout.activity_main, null)
+//        setContentView(view)
+//        setContentView(R.layout.activity_main)
+
+        buildUI()
+
+    }
+
+    fun buildUI() {
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.VERTICAL
+        setContentView(layout)
+
+        for (i in 0..10) {
+            val btn = Button(this)
+            btn.text = "Remove me " + i
+            btn.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            layout.addView(btn)
+            btn.setOnClickListener(this)
+        }
+    }
+}
