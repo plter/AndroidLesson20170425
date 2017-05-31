01d02e6528b2ac5500b9606f06f30bb51bdb249e
plter <xtiqin@163.com>
2017-05-31 15:42:43 +0800

    使用摄像机

--- 20170531/OpenCamera/app/src/main/java/top/yunp/opencamera/MainActivity.java
+++ 20170531/OpenCamera/app/src/main/java/top/yunp/opencamera/MainActivity.java
@@ -4,31 +4,45 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.databinding.DataBindingUtil;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Surface;
+import android.view.TextureView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+import top.yunp.opencamera.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
 
 
     private static final int REQUEST_TO_OPEN_CAMERA = 1;
     private CameraManager cameraManager;
+    private ActivityMainBinding binding;
+    private CameraDevice cameraDevice;
+    private CaptureRequest.Builder captureRequestBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
 //        if (VERSION.SDK_INT >= 23) {
 //            System.out.println(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
 //        }
 
         cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
-        openCamera();
+
+        binding.cameraPreview.setSurfaceTextureListener(this);
     }
 
 
@@ -77,7 +91,21 @@ public class MainActivity extends AppCompatActivity {
     private final CameraDevice.StateCallback openCameraStateCallback = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(@NonNull CameraDevice camera) {
-            System.out.println(camera);
+
+            cameraDevice = camera;
+            SurfaceTexture surfaceTexture = binding.cameraPreview.getSurfaceTexture();
+            surfaceTexture.setDefaultBufferSize(500, 800);
+
+            Surface surface = new Surface(surfaceTexture);
+
+            try {
+                captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                captureRequestBuilder.addTarget(surface);
+
+                camera.createCaptureSession(Arrays.asList(surface), cameraSessionStateCallback, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -90,4 +118,43 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+    private CameraCaptureSession.StateCallback cameraSessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession session) {
+            try {
+                session.setRepeatingRequest(captureRequestBuilder.build(), cameraSessionCaptureCallback, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+        }
+    };
+
+    private CameraCaptureSession.CaptureCallback cameraSessionCaptureCallback = new CameraCaptureSession.CaptureCallback() {
+    };
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        openCamera();
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
 }
