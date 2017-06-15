b755b25fa1d61a56045ab49d938ba59d8b89ee6a
plter <xtiqin@163.com>
2017-06-15 11:48:50 +0800

    字符串基本操作

--- 20170625/strings/main.c
+++ 20170625/strings/main.c
@@ -3,18 +3,48 @@
 
 int main() {
 
+    //define string
 //    char *str = "Hello World";
 
-    char str[20];
-    str[0] = 'H';
-    str[1] = 'e';
-    str[2] = 'l';
-    str[3] = 'l';
-    str[4] = 'o';
-    str[5] = 0;
-    str[6] = 'W';
-    str[7] = 'o';
+//    char str[20];
+//    str[0] = 'H';
+//    str[1] = 'e';
+//    str[2] = 'l';
+//    str[3] = 'l';
+//    str[4] = 'o';
+//    str[5] = 0;
+//    str[6] = 'W';
+//    str[7] = 'o';
+//
+//    printf("str:%s,len:%zd\n", str, strlen(str));
 
-    printf("str:%s,len:%zd\n", str, strlen(str));
+
+    //contact string
+    char str[20];
+//    strcat(str, "Hello");
+//    strcat(str, "World");
+    sprintf(str, "Hello %d", 100);
+
+    size_t strLength = strlen(str);
+
+    printf("%s\n", str);
+    printf("size of str :%zd,and the string length is :%zd\n", sizeof(str), strlen(str));
+
+//    size_t strLength = strlen(str);
+
+    for (int i = 0; i < strLength; ++i) {
+        printf("%c\n", str[i]);
+    }
+
+//    printf("%c\n", *str);
+
+    char *cp = str;
+    char c;
+    while ((c = *cp) > 0) {
+        printf("%c\n", c);
+
+        cp++;
+    }
+
     return 0;
 }
